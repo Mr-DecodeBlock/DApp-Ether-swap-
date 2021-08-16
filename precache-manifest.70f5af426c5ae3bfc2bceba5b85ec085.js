a4bbdbd4d1c0e69416c990e555edfd0a9e269051
chanceDira <chancdesire60@gmail.com>
2021-08-16 18:00:19 +0200

    Updates

---
+++ precache-manifest.70f5af426c5ae3bfc2bceba5b85ec085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d55cf7c23f3eb1a9715",
+    "url": "/EthSwap-Blockchain/static/css/main.7a3fe424.chunk.css"
+  },
+  {
+    "revision": "4d55cf7c23f3eb1a9715",
+    "url": "/EthSwap-Blockchain/static/js/main.4d55cf7c.chunk.js"
+  },
+  {
+    "revision": "788a9d592e7c9b70f067",
+    "url": "/EthSwap-Blockchain/static/css/1.86e4ef1b.chunk.css"
+  },
+  {
+    "revision": "788a9d592e7c9b70f067",
+    "url": "/EthSwap-Blockchain/static/js/1.788a9d59.chunk.js"
+  },
+  {
+    "revision": "8946b9e00cc856c7a2a8",
+    "url": "/EthSwap-Blockchain/static/js/runtime~main.8946b9e0.js"
+  },
+  {
+    "revision": "ffcc2a32d699c3feff93f50c686da76a",
+    "url": "/EthSwap-Blockchain/index.html"
+  }
+];
